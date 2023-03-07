b3e101d332e17e2a6813b206f8256a607caef05a
TheForestPerson <116846756+TheForestPerson@users.noreply.github.com>
2023-03-07 21:53:35 +0100

    Create index.html, rock-paper-scissors.js, README.md and write first draft of pseudocode

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rock_paper_scissors
+This project will consist of javascript code
+written to play rock paper scissors against 
+the computer in the browser console.
+It will be written from scratch and will serve 
+as practice for js syntax, writing functions, debugging and writing 
+pseudocode to create a plan before starting to code.
---
+++ rock-paper-scissors.js
@@ -0,0 +1,11 @@
+//play game of rock-paper-scissors against computer
+
+//get computer choice
+    //get random num between 1 and 3
+    //assign each num to a possible play
+    //return play
+//get your choice
+    //either direct code input or promt
+//compare both choices
+    //somehow tell the function witch play is stronger
+    //return string with winner and result
