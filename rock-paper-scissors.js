3efef97306dc9749643a397797a06b7edd0a137c
TheForestPerson <116846756+TheForestPerson@users.noreply.github.com>
2023-03-12 21:23:41 +0100

    Create function playRound in rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,29 +10,50 @@
 function getComputerChoice() {
     let x = Math.floor(Math.random() * (3) + 1)
     if (x === 1) {
-        return "Rock";
+        return "rock";
     } else if (x === 2) {
-        return "Paper";
+        return "paper";
     } else if (x == 3) {
-        return "Scissors";
+        return "scissors";
     } else {
         return "Something went wrong";
     }
 }
- console.log(getComputerChoice())
+ //console.log(getComputerChoice())
 
 
    
 
 //compare both choices
-    //somehow tell the function witch play is stronger
+    //make playerSelection lowerCase
+    //make if else statements for every possible play
     //return string with winner and result
 
 function playRound(playerSelection,computerSelection) {
-
+    let me = playerSelection.toLowerCase();
+    let pc = computerSelection;
+    if (me === pc) {
+        return "It's a tie!";
+    } else if ((me === "rock") && (pc === "paper")){
+        return "You lose! Paper beats Rock";
+    } else if ((me === "rock") && (pc === "scissors")){
+        return "You win! Rock beats Scissors" ;
+    } else if ((me === "paper") && (pc === "rock")){
+        return "You win! Paper beats Rock";
+    } else if ((me === "paper") && (pc === "scissors")){
+        return "You lose! Scissors beat Paper";
+    } else if ((me === "scissors") && (pc === "rock")){
+        return "You lose! Rock beats Scissors";
+    } else if ((me === "scissors") && (pc === "paper")){
+        return "You win! Scissors beat paper";
+    } else {
+        return "Something went wrong!";
+    }
 }
 
-
+let playerSelection = "papeR";
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
 
 //get your choice
     //either direct code input or promt
