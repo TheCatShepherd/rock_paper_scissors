ea914343f27f367a8886f39fbaa91d06cbb69c63
TheForestPerson <116846756+TheForestPerson@users.noreply.github.com>
2023-03-12 14:09:30 +0100

    Create function getComputerChoice in rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,9 +3,37 @@
 //get computer choice
     //get random num between 1 and 3
     //assign each num to a possible play
-    //return play
-//get your choice
-    //either direct code input or promt
+        //function that calls getRandomNum,
+        //depending on the result, return rock, paper or scissors
+        //return play
+
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * (3) + 1)
+    if (x === 1) {
+        return "Rock";
+    } else if (x === 2) {
+        return "Paper";
+    } else if (x == 3) {
+        return "Scissors";
+    } else {
+        return "Something went wrong";
+    }
+}
+ console.log(getComputerChoice())
+
+
+   
+
 //compare both choices
     //somehow tell the function witch play is stronger
     //return string with winner and result
+
+function playRound(playerSelection,computerSelection) {
+
+}
+
+
+
+//get your choice
+    //either direct code input or promt
+    //first user input by declaring const
